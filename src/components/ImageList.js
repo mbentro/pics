f5afd2a54255d72e048173d3bd6ca66411a5adbd
Matt Bentrovato <mbentrovato@atb.com>
2019-03-23 13:18:10 -0600

    Pic Search Complete

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  let images = props.images.map(({ description, id, urls }) => {
-    return <img src={urls.small} key={id} alt={description} />;
+  let images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
